04192c19cec0f968ff2b7c5118ee4be994175d54
systemthinker <robbertoutenaar@gmail.com>
2020-06-20 20:32:54 +0200

    Styling for products

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -7,7 +7,7 @@ export default function ProductCard({id, name, description, price, imageUrl}) {
 
         <Link to={`/product/${id}`} style={{ textDecoration: 'none' }}>
         <div className="card">
-            <img height="300" src={imageUrl} alt=""></img>
+            <img height="200" width="180" className="center-block" src={imageUrl} alt=""></img>
             <p>{name}</p>
             <p>{description}</p>
             <p ><bolder className="price">{price}</bolder></p>
