3d5fa15ef64fdd44ba91238a3303f804832cfdcb
systemthinker <robbertoutenaar@gmail.com>
2020-06-26 10:50:38 +0200

    added shopping card

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,23 +3,57 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectToken } from "../../store/clients/selectors";
+import { selectToken,selectClient } from "../../store/clients/selectors";
+import { selectBasket } from '../../store/baskets/selectors'
 import NavbarItem from "./NavbarItem";
 import LoggedIn from "./LoggedIn";
 import LoggedOut from "./LoggedOut";
 import './index.css'
+import { FaBeer, FaShoppingCart } from 'react-icons/fa';
+import { IconContext } from "react-icons";
 
 export default function Navigation() {
   const token = useSelector(selectToken);
+  const client = useSelector(selectClient)
+  const basket = useSelector(selectBasket)
+  const clientId = client.id
+  console.log('basket is', basket)
+  console.log('basketL' ,basket.length)
+  function counterValue(){
+    if(basket.length === 0){
+      return null
+    } else {
+      return basket.length;
+    }
+  }
   // add token for admin later
   // add logic for showing basket later if needed
   
   const loginLogoutControls = token ? <LoggedIn /> : <LoggedOut />;
+  const shoppingCardControls = token ?   <Navbar.Brand as={NavLink} to={`/basket/${clientId}`}>
+                                                      <IconContext.Provider value={{ color: "blue", className: "global-class-name" }}>
+                                                        <div>
+                                                              <FaShoppingCart size={80} value={{ style: { className: 'react-icons' } }}/>
+                                                              <h4 className="counter">{counterValue()}</h4>
+                                                        </div>
+                                                    </IconContext.Provider>
+                                      </Navbar.Brand>
+  
+  : <Navbar.Brand>
+  <IconContext.Provider value={{ color: "blue", className: "global-class-name" }}>
+    <div>
+          <FaShoppingCart size={80} value={{ style: { className: 'react-icons' } }}/>
+          
+    </div>
+</IconContext.Provider>
+</Navbar.Brand>
 
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand as={NavLink} to="/">
         <img src={`${'http://www.robbertvandenoutenaar.com/images/logoSuusjePluusje.png'}`} alt="" height="140px" width="250px"/>
+        
+        
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
@@ -27,8 +61,10 @@ export default function Navigation() {
           <NavbarItem className="nav-item" path="/" linkText="Home" />
           <NavbarItem path="/about" linkText="Over" />
           <NavbarItem path="/info" linkText="Meer Info"/>
+          {shoppingCardControls}
           {loginLogoutControls}
-          <NavbarItem path="/basket" linkText="Mijn Winkelwagen"/>
+          
+          
         </Nav>
       </Navbar.Collapse>
     </Navbar>
