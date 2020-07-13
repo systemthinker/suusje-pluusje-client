8c33b3fe0df4fa18743a05ba6fca57180d35f697
systemthinker <robbertoutenaar@gmail.com>
2020-07-13 16:31:30 +0200

    changed error message for email

--- src/components/FormErrorMessages/Email/index.js
+++ src/components/FormErrorMessages/Email/index.js
@@ -1,12 +1,12 @@
-import '../index.css'
-import { Form } from 'react-bootstrap'
+// import '../index.css'
+// import { Form } from 'react-bootstrap'
 
-export default function Email() {
-    return (
+// export default function Email() {
+//     return (
        
-       <Form.Text>
-            <span className="spanErrorMessage">Vul alstublieft een gelding emailadres in</span>
-        </Form.Text>
+//        <Form.Text>
+//             <span className="spanErrorMessage">Vul alstublieft een gelding emailadres in</span>
+//         </Form.Text>
        
-    )
-}
+//     )
+// }
