6809d1db19d6bc6fe487ff1d5b75eb25084e0cd8
systemthinker <robbertoutenaar@gmail.com>
2020-08-02 12:56:10 +0200

    fixed error with loading time heroku

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,6 @@
 export const apiUrl =
-  process.env.API_URL ||
-  "https://suusje-server.herokuapp.com" ||
-  "http://localhost:4000";
+  process.env.API_URL || "https://suusje-server.herokuapp.com";
+
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const deliveryCosts = 5.95;
 export const productIdForExtraProductsOne = 12;
