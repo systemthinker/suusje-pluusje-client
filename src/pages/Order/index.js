a707881d3deab8e7aa1013f831d330056f6734f4
systemthinker <robbertoutenaar@gmail.com>
2020-09-06 14:50:08 +0200

    added logic for order-postalCodeApi feature

--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row, Form } from "react-bootstrap";
 import AddressOrderCard from "../../components/AddressOrderCard";
 
 export default function Order() {
@@ -7,6 +7,11 @@ export default function Order() {
     <div>
       <Container fluid>
         <h1>Bestelling</h1>
+        <div key={`inline-radio`} className="mb-3">
+          <Form.Check inline type="radio" id={`inline-radio-1`} />
+          <Form.Check inline type="radio" id={`inline-radio-2`} />
+          <Form.Check inline type="radio" id={`inline-radio-3`} />
+        </div>
 
         <AddressOrderCard />
       </Container>
