49a57520f2641eef6be9f5465bdf5f9f5ce5a98b
systemthinker <robbertoutenaar@gmail.com>
2020-09-20 13:54:33 +0200

    small spelling error

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ export const apiUrl = "https://suusje-server.herokuapp.com";
 
 // process.env.API_URL ||
 
-export const siteUrl = "http://suusje-pluusje.netlify.app";
+export const siteUrl = "https://suusje-pluusje.netlify.app";
 
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const deliveryCosts = 5.95;
