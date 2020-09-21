a510c83df0c1c59cdb7976c970c079a7436c40b2
systemthinker <robbertoutenaar@gmail.com>
2020-09-21 14:23:29 +0200

    cleanup

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const apiUrl = " https://suusje-server.herokuapp.com";
+export const apiUrl = "https://suusje-server.herokuapp.com";
 
 // "https://suusje-server.herokuapp.com"
 // "http://localhost:4000"
